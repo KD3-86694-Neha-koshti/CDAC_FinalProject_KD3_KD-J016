7e09a6df3d854cfbad68a230e8c805f516f57454
86694-Neha-koshti <nehakoshti23@gmail.com>
2025-01-27 15:57:18 +0530

    pojo creation

--- spring_boot_backend_template/src/main/java/com/blogs/pojos/GasDelivery.java
+++ spring_boot_backend_template/src/main/java/com/blogs/pojos/GasDelivery.java
@@ -1,5 +1,51 @@
 package com.blogs.pojos;
 
-public class GasDelivery extends BaseEntity{
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name="order_gas")
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString(callSuper = true,exclude = {"bookId","agentId"})
+public class GasDelivery extends BaseEntity
+{
+	@ManyToOne
+	@JoinColumn(name="agent_id")
+	private User agentId;
+	
+	@ManyToOne
+	@JoinColumn(name="book_id")
+	private Booking bookId;
+	
+	private Status status;
+	
+	@Column(name="payment_status")
+	private boolean paymnetStatus;
+
+	
+	public enum Status
+	{
+		ASSIGNED,DELIVERED
+	}
+
+
+	public GasDelivery(User agentId, Booking bookId, Status status, boolean paymnetStatus) {
+		super();
+		this.agentId = agentId;
+		this.bookId = bookId;
+		this.status = status;
+		this.paymnetStatus = paymnetStatus;
+	}
+
 
 }
--- spring_boot_backend_template/src/main/java/com/blogs/pojos/User.java
+++ spring_boot_backend_template/src/main/java/com/blogs/pojos/User.java
@@ -1,6 +1,65 @@
 package com.blogs.pojos;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name="user")
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString(callSuper = true,exclude = "bookingId")
 public class User extends BaseEntity
 {
+	@Column(name="full_name")
+	private String fullName;
 	
+	@Column(length=50,unique = true)
+	private String email;
+	
+	@Column(length = 25,nullable = false)
+	private String password;
+	
+	private String address;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(length = 40)
+	private UserRole role;
+	
+	@Column(name="mobile_no")
+	private Long mobileNo;
+	
+	@OneToMany(mappedBy = "userID",cascade = CascadeType.ALL,orphanRemoval = true)
+	private List<Booking> bookingId;
+
+	@OneToMany(mappedBy = "agentId",cascade = CascadeType.ALL,orphanRemoval = true)
+	private List<GasDelivery> cyliderId;
+	
+	public User(String fullName, String email, String password, String address, UserRole role, Long mobileNo,
+			List<Booking> bookingId) {
+		super();
+		this.fullName = fullName;
+		this.email = email;
+		this.password = password;
+		this.address = address;
+		this.role = role;
+		this.mobileNo = mobileNo;
+		this.bookingId = bookingId;
+	}
+
+
+	public enum UserRole{
+		ADMIN,CUSTOMER,DELEVRYAGENT
+	}
 }
